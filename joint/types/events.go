e409a2399ecaa67ad1fcd2eaa77f3017b47c0d91
Srinivas Baride <srini@exidio.co>
2020-10-18 16:45:57 +0530

    Added errors and events

---
+++ joint/types/events.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	EventTypeSetCount = "set_count"
+	EventTypeSet      = "set"
+	EventTypeDeposit  = "deposit"
+)
+
+const (
+	AttributeKeyCount    = "count"
+	AttributeKeyIdentity = "identity"
+	AttributeKeyConsents = "consents"
+)
