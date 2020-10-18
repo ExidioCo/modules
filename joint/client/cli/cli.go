08b7da86888ef3d069af5fdb0b1746406d4e4c02
Srinivas Baride <srini@exidio.co>
2020-10-18 16:57:16 +0530

    Added CLI commands

---
+++ joint/client/cli/cli.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/spf13/cobra"
+)
+
+func GetQueryCmd(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "joint",
+		Short: "Joint account module sub-commands",
+	}
+
+	cmd.AddCommand(flags.GetCommands(
+		queryAccount(cdc),
+		queryAccounts(cdc),
+	)...)
+
+	return cmd
+}
+
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "joint",
+		Short: "Joint account module sub-commands",
+	}
+
+	cmd.AddCommand(flags.PostCommands(
+		txCreate(cdc),
+		txDeposit(cdc),
+	)...)
+
+	return cmd
+}
