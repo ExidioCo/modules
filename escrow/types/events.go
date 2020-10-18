6ca4bb9ad3854b4749f3434108265ff32f829d7f
Srinivas Baride <srini@exidio.co>
2020-10-18 17:29:58 +0530

    Added errors and events

---
+++ escrow/types/events.go
@@ -0,0 +1,17 @@
+package types
+
+const (
+	EventTypeSetCount = "set_count"
+	EventTypeSet      = "set"
+	EventTypeApprove  = "approve"
+)
+
+const (
+	AttributeKeyCount    = "count"
+	AttributeKeyIdentity = "identity"
+	AttributeKeyFrom     = "from"
+	AttributeKeyTo       = "to"
+	AttributeKeyCoins    = "coins"
+	AttributeKeyDeadline = "deadline"
+	AttributeKeyHolder   = "holder"
+)
